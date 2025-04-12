c72414bf4347f1941ed94a805867991d90949adf
mmhrabah <mmhrabah@gmail.com>
2025-04-12 11:09:04 +0200

    Project Template V2

---
+++ Assets/_Project/Scripts/Utils/UI/Buttons/NextButton.cs
@@ -0,0 +1,32 @@
+using Michsky.MUIP;
+using Rabah.Utils.UI;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Rabah.Scenes.Definitions.UI
+{
+    public class NextButton : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent onClickInSreenWithNoneNext;
+        public void NextButtonOnClickEvent()
+        {
+            if (Utils.UI.UIManager.Instance.CurrentScreen.IsScreenDataValid())
+            {
+                ScreenHandle next = Utils.UI.UIManager.Instance.CurrentScreen.Next;
+                if (next == ScreenHandle.None)
+                {
+                    onClickInSreenWithNoneNext?.Invoke();
+                    return;
+                }
+                Utils.UI.UIManager.Instance.OpenScreen(next);
+            }
+            else
+            {
+                Utils.UI.UIManager.Instance.ShowNotificationModal(
+                    "Missing Data",
+                    "To go to next steps you must have at least one element in each Definition"
+                );
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Utils/UI/UIManager.cs
@@ -0,0 +1,274 @@
+using Rabah.Scenes.Definitions.UI;
+using System.Collections;
+using System.Collections.Generic;
+using Michsky.MUIP;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rabah.Utils.UI
+{
+    public class UIManager : BaseSingleton<UIManager>
+    {
+        [SerializeField]
+        [Tooltip("All windows will be instantiated under this this parent")]
+        private RectTransform windowsParent;
+        [Space(5)]
+        [SerializeField]
+        [Tooltip("All windows has main data [handle - prefab - next - previous]")]
+        private List<ScreenPrefab> screenPrefabs;
+
+        [Space(5)]
+        [Header("Buttons")]
+        [SerializeField]
+        private BackButton backButton;
+        [SerializeField]
+        private ButtonManager resetButton;
+        [SerializeField]
+        private NextButton nextButton;
+
+        [Space(5)]
+        [Header("Screen Shared Elements")]
+        [SerializeField]
+        private Canvas canvas;
+        [SerializeField]
+        private Camera mainCamera;
+        [SerializeField]
+        private GameObject loading;
+        [SerializeField]
+        private TMP_Text screenTitleTMP_Text;
+
+        [Space(5)]
+        [Header("Screen Backgrounds")]
+        [SerializeField]
+        private Image mainBackground;
+        [SerializeField]
+        private Image changeableBackground;
+        [SerializeField]
+        private List<Sprite> changeableBackgroundSprites;
+
+        [Space(5)]
+        [Header("Screen Layout")]
+        [SerializeField]
+        private GameObject topPanel;
+        [SerializeField]
+        private GameObject bottomPanel;
+
+        [Space(5)]
+        [Header("Notifications")]
+        [SerializeField]
+        private ModalWindowManager notificationModalViewManager;
+
+        private Dictionary<ScreenHandle, Screen> prefabDictionary = new();
+        private Dictionary<ScreenHandle, Screen> allScreens = new();
+        private Screen currentScreen;
+        private readonly Vector2 windowsRectAnchorMin = new(0, 0);
+        private readonly Vector2 windowsRectAnchorMax = new(1, 1);
+        private readonly Vector2 windowsRectOffsetMin = new(0, 125);
+        private readonly Vector2 windowsRectOffsetMax = new(0, -150);
+        private int changeableBGIndex = 0;
+        public Screen CurrentScreen { get => currentScreen; set => currentScreen = value; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            foreach (var screenPrefab in screenPrefabs)
+            {
+                if (!prefabDictionary.ContainsKey(screenPrefab.handle))
+                {
+                    prefabDictionary.Add(screenPrefab.handle, screenPrefab.prefab);
+                    allScreens.Add(screenPrefab.handle, screenPrefab.prefab);
+                }
+            }
+            if (screenPrefabs.Count > 0)
+            {
+                var firstScreen = screenPrefabs[0];
+                OpenScreen(firstScreen.handle);
+            }
+        }
+
+        public void ShowLoading()
+        {
+            loading.SetActive(true);
+        }
+
+        public void HideLoading()
+        {
+            loading.SetActive(false);
+        }
+
+        public void OpenScreen(ScreenHandle handle, bool closePreviousScreen = true, ScreenData data = null, bool useFadeAnimation = true)
+        {
+            ++changeableBGIndex;
+            if (changeableBackgroundSprites.Count > 0)
+            {
+                changeableBGIndex %= changeableBackgroundSprites.Count;
+                changeableBackground.sprite = changeableBackgroundSprites[changeableBGIndex];
+            }
+            if (prefabDictionary.TryGetValue(handle, out var prefab))
+            {
+                if (closePreviousScreen)
+                {
+                    CloseScreen(useFadeAnimation);
+                }
+
+                var screenInstance = Instantiate(prefab, windowsParent.transform);
+                CurrentScreen = screenInstance;
+                CurrentScreen.SetupLayout();
+                CurrentScreen.ControlScreenMainContent(false);
+                canvas.renderMode = screenInstance.RenderMode;
+                screenTitleTMP_Text.text = CurrentScreen.Title;
+                if (useFadeAnimation)
+                {
+                    CurrentScreen.FadeInScreenContent(() =>
+                    {
+                        screenInstance.OnOpen(data);
+                    });
+                }
+                else
+                {
+                    screenInstance.OnOpen(data);
+                }
+            }
+            else
+            {
+                Debug.LogError($"No prefab found for screen handle: {handle}");
+            }
+        }
+
+        public void CloseScreen(bool useFadeAnimation)
+        {
+            if (currentScreen == null)
+            {
+                Debug.LogWarning("No active screen found to close");
+                return;
+            }
+            if (useFadeAnimation)
+            {
+                Screen screenWillClose = currentScreen;
+                screenWillClose.FadeOutScreenContent(() =>
+                {
+                    screenWillClose.OnClose();
+                    Destroy(screenWillClose.gameObject);
+                });
+            }
+            else
+            {
+                currentScreen.OnClose();
+                Destroy(currentScreen.gameObject);
+            }
+        }
+
+        public void CloseScreen(Screen screen)
+        {
+            screen.OnClose();
+            Destroy(screen.gameObject);
+        }
+
+        public void ResetScreen(Screen screen)
+        {
+            screen.OnReset();
+        }
+
+        public void ResetCurrentScreen()
+        {
+            currentScreen.OnReset();
+        }
+
+        public Screen GetScreen(ScreenHandle handle)
+        {
+            if (allScreens.TryGetValue(handle, out var screen))
+            {
+                return screen;
+            }
+
+            Debug.LogWarning($"No active screen found for handle: {handle}");
+            return null;
+        }
+
+        public void ControlNextButton(bool isActive)
+        {
+            nextButton.gameObject.SetActive(isActive);
+        }
+
+        public void ControlBackButton(bool isActive)
+        {
+            backButton.gameObject.SetActive(isActive);
+        }
+
+        public void ControlBottomPanel(bool isActive)
+        {
+            bottomPanel.SetActive(isActive);
+        }
+
+        public void ControlCamera(bool isActive)
+        {
+            mainCamera.gameObject.SetActive(isActive);
+        }
+
+        public void AddItemsToBottomPanel(RectTransform item, int siblingIndex)
+        {
+            var itemName = item.name;
+            item.name = $"{itemName} - {CurrentScreen.name}";
+            item.SetParent(bottomPanel.transform);
+            item.SetSiblingIndex(siblingIndex);
+        }
+
+        public void ControlBackground(bool isActive)
+        {
+            mainBackground.gameObject.SetActive(isActive);
+        }
+
+
+        public void ShowComingSoonModal()
+        {
+            ShowNotificationModal("Coming Soon", "This feature is not available yet.");
+        }
+
+        public void ShowNotificationModal(string title, string descriptionText)
+        {
+            notificationModalViewManager.titleText = title;
+            notificationModalViewManager.descriptionText = descriptionText;
+            notificationModalViewManager.UpdateUI();
+            notificationModalViewManager.OpenWindow();
+        }
+
+        public void RefreshCanvas(RectTransform rectTransform)
+        {
+            StartCoroutine(RefreshCanvasCoroutine(rectTransform));
+        }
+
+        private IEnumerator RefreshCanvasCoroutine(RectTransform rectTransform)
+        {
+            yield return new WaitForEndOfFrame();
+            var pos = rectTransform.sizeDelta;
+            rectTransform.sizeDelta = new() { x = pos.x + 0.05f, y = pos.y + 0.05f };
+            yield return new WaitForEndOfFrame();
+            rectTransform.sizeDelta = pos;
+        }
+
+        public void ControlTopPanel(bool isActive)
+        {
+            topPanel.SetActive(isActive);
+        }
+
+        public void SetWindowsRectData(Vector2 anchorMin, Vector2 anchorMax, Vector2 offsetMin, Vector2 offsetMax)
+        {
+            windowsParent.anchorMin = anchorMin;
+            windowsParent.anchorMax = anchorMax;
+            windowsParent.offsetMin = offsetMin;
+            windowsParent.offsetMax = offsetMax;
+        }
+
+        public void ResetWindowsRectData()
+        {
+            SetWindowsRectData(windowsRectAnchorMin, windowsRectAnchorMax, windowsRectOffsetMin, windowsRectOffsetMax);
+        }
+
+        public void ControlResetButton(bool isActive)
+        {
+            resetButton.gameObject.SetActive(isActive);
+        }
+    }
+}
